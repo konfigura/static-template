d841036130e52e0a74eaf38331ea0863ca624a24
Krystian <krystian@konfigura.studio>
2023-10-02 10:19:50 +0200

    Added CSS loaders and PostCSS plugins.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const postCSSPlugins = [
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
+];
 
 module.exports = {
   entry: './app/assets/scripts/App.js',
@@ -7,5 +12,27 @@ module.exports = {
     path: path.resolve(__dirname, 'app')
   },
   mode: 'development',
-  watch: true
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {url: false}
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: postCSSPlugins
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
